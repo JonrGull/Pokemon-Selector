436d70e271388e94c0aa1ae2e3164512662f001b
Jon <jonrgull@gmail.com>
2021-08-17 21:52:24 +0900

    changed name of parameter from data -> pokemonObject.

--- data/script.js
+++ data/script.js
@@ -8,12 +8,13 @@ fetch("/data/pokemonData.json") // fetch creates a Promise
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
+  .then(function (pokemonObject) {
+    // is pokemonObject okay? May be too long?
     // makes the entire object show up in the log
-    console.log(data);
-    theID = data.pokemon[0].id;
-    theNumber = data.pokemon[0].num;
-    theName = data.pokemon[0].name;
+    console.log(pokemonObject);
+    theID = pokemonObject.pokemon[0].id;
+    theNumber = pokemonObject.pokemon[0].num;
+    theName = pokemonObject.pokemon[0].name;
 
     // gets the first pokemon's details. This is an example, I don't think we can use it like this here.
     console.log(theID);
