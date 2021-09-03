ebdeea136b259a4130079d5be477eb46c9972880
Jon Gull <jonrgull@gmail.com>
2021-09-03 18:22:11 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { useState } from "react";
 import "./App.css";
-
 function App() {
-  return <div>Pokemon time</div>;
+  const value = useState();
+  console.log(value);
+
+  return (
+    <div>
+      <h1>Is state important to know? Yes</h1>
+    </div>
+  );
 }
 
 export default App;
