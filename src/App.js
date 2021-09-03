adcf14040df5d27c184303668511626379040e02
Jon Gull <jonrgull@gmail.com>
2021-09-03 22:00:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
 function App() {
-  const value = useState();
-  console.log(value);
-
   return (
     <div>
       <h1>Yo this is React! Testing testing.</h1>
