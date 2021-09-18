e253a46eddd530ca1216cb5c46322640443bd70b
Jon Gull <jonrgull@gmail.com>
2021-09-18 21:33:45 +0900

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
-function App() {
+
+
+export default function App() {
+
+
   return (
     <div>
       <h1>Pokemon Selector</h1>
     </div>
-  );
-}
+  )
 
-export default App;
+
+  }
