6b570271201e4552e605b116d818cab04e708dfb
Jon Gull <jonrgull@gmail.com>
2021-09-21 13:14:22 +0900

    changed name to pokemonlist for clarity

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import PokemonList from "./data/PokemonList";
 
 export default function App() {
+  const [pokemonData, setPokemonData] = React.useState(null);
 
-
+  React.useEffect(() => {
+    setPokemonData(PokemonList.runways[0].name);
+  }, []);
   return (
     <div>
       <h1>Pokemon Selector</h1>
     </div>
-  )
-
-
-  }
+  );
+}
