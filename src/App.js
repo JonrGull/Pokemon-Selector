52b3b73bbc523a43b5f08e3d10ba6e776f04cb2e
Jon Gull <jonrgull@gmail.com>
2021-09-24 17:10:21 +0900

    Update App.js
    lots of notes to try and make a global array to mutate

--- src/App.js
+++ src/App.js
@@ -3,32 +3,54 @@ import "./App.css";
 import PokemonLibrary from "./data/PokemonList.json";
 
 export default function App() {
-  const [pokemonData, setPokemonData] = React.useState(PokemonLibrary.pokemon);
+  const [pokemonData, setPokemonData] = React.useState(PokemonLibrary.pokemon); //exact  same value as myFilteredPoke. Just the starting value
 
-  const toDisplay = PokemonLibrary.pokemon;
+  const libraryCopy = PokemonLibrary.pokemon; //toDisplay
 
   React.useEffect(() => {
     setPokemonData(PokemonLibrary.pokemon);
   }, []);
 
-  const filterPokemon = (e) => {
-    const myFilteredPoke = toDisplay.filter((f) => {
-      if (f.type[1] === "Ice") {
-        //maybe use .includes to check both index spots ["grass","poison"] vs ["poison","grass"]
-        //if dealing with an array here you need the index
-        return true;
-      } else {
-        return false;
-      }
+  //const globalArray = giant array of pokemon objects
+  // have functions that mutate globalArray and returns the array to global memory
+  // so other functions call pass this mutated array and spit out the mutated array a 2nd time
+
+  //how do we make a global array, that can be modified and saved
+
+  /*   let arr = PokemonLibrary.pokemon.filter(function (e) {
+    console.log(e.type.indexOf(type) >= 0);
+  }); 
+  */
+
+  let myFilteredPoke = libraryCopy;
+
+  const filterTypeOne = (e) => {
+    myFilteredPoke = libraryCopy.filter((pokeType) => {
+      //myFilteredPoke should be an ARRAY of OBJECTS
+
+      return pokeType.type.includes("Water");
     });
+    console.log(myFilteredPoke.splice); // shows array of objects of left over pokemon
+
     setPokemonData(myFilteredPoke); //always need this. This re-renders to update state.
+  };
+
+  // we need to have one global array to pass to all the functions
+  const filterTypeTwo = (e) => {
+    myFilteredPoke = libraryCopy.filter((pokeType) => {
+      return pokeType.type.includes("Poison");
+    });
+    console.log(myFilteredPoke); // shows array of objects of left over pokemon
+
+    setPokemonData(myFilteredPoke);
   };
 
   return (
     <div className="App">
       <h1>Pokemon Selector!</h1>
       <div>
-        <button onClick={filterPokemon}>Filter me</button>
+        <button onClick={filterTypeOne}>Filter water</button>
+        <button onClick={filterTypeTwo}>Filter poison</button>
       </div>
       {pokemonData &&
         pokemonData.map((poke) => (
@@ -40,3 +62,26 @@ export default function App() {
     </div>
   );
 }
+
+/* 
+
+const globalArray = [1,2,3,4,5,6,7,8,9,10]
+
+<button> filter even            </button>
+<button> filters numbers over 6 </button>
+
+function filterEvens(){
+  let results = []
+
+  for(let nums of globalArray){
+    if(nums % 2 === 0){
+      result.push(nums)
+    }
+  }
+  return results // this results should be a array of all evens
+}
+
+
+
+
+*/
