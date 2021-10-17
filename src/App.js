d4651a83578be179abbaac26c1473260ebbe0ebd
Jon <jonrgull@gmail.com>
2021-10-17 17:48:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import DisplayPokemon from "./components/DisplayPokemon";
 import TypeButtons from "./components/TypeButtons";
 import "./App.css";
