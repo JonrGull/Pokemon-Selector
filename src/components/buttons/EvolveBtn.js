4345d436cc6d020482b19a2ea1dcb4727ef23043
Jon <jonrgull@gmail.com>
2021-11-02 16:45:59 +0900

    Added comments

--- src/components/buttons/EvolveBtn.js
+++ src/components/buttons/EvolveBtn.js
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 // import Stack from "@mui/material/Stack";
 
 export default function EvolveBtn({ mapEvolutions, onClick }) {
-  //show both buttons if mapEvolutions has a mix of true and false.
+  //.some() is used to show both buttons if mapEvolutions has a mix of true and false.
 
   //pokemon that has a property prev_evolution or next_evolution
   const canEvolve = mapEvolutions.some((v) => v === true) ? (
